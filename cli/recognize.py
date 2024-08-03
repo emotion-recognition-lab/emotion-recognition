aa5ee9bfb928a53f2815cb67cc453faeaa0a04f0
zrr1999 <2742392377@qq.com>
2024-08-03 19:32:46 +0800

    ✨ feat: support distill

--- cli/recognize.py
+++ cli/recognize.py
@@ -29,13 +29,27 @@ from recognize.model import (
 )
 from recognize.module.fusion import gen_fusion_layer
 from recognize.typing import LogLevel, ModalType
-from recognize.utils import find_best_model, load_best_model, train_and_eval
+from recognize.utils import find_best_model, load_best_model, train_and_eval, train_and_eval_distill
 
 
 def init_logger(log_level: LogLevel):
     handler = RichHandler(highlighter=NullHighlighter(), markup=True)
     logger.remove()
     logger.add(handler, format="{message}", level=log_level)
+
+
+def seed_everything(seed):
+    import random
+
+    import numpy as np
+
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def provide_meld_datasets(
@@ -65,10 +79,12 @@ def provide_meld_datasets(
     return train_dataset, dev_dataset, test_dataset
 
 
-def clean_cache():
-    for file in os.listdir("./cache"):
-        file_path = os.path.join("./cache", file)
-        os.unlink(file_path)
+def symlink(src: Path | str, dst: Path | str, target_is_directory: bool = False):
+    dst = Path(dst)
+    src = Path(src)
+    dst.unlink(missing_ok=True)
+    logger.info(f"Create symlink: {dst} -> {src}")
+    dst.symlink_to(src.absolute(), target_is_directory=target_is_directory)
 
 
 def generate_model_label(modal: list[ModalType], freeze: bool, label_type: str):
@@ -88,6 +104,115 @@ app = typer.Typer(pretty_exceptions_show_locals=False)
 
 
 @app.command()
+def distill(
+    teacher_checkpoint: Path,
+    config_path: Path = Path("configs/default.toml"),
+) -> None:
+    """
+    When using knowledge distillation,
+    it is essential to ensure that the preprocessor for both the teacher model and the student model is the same.
+    Generally, you can use the one corresponding to the student model,
+    as the student model typically has more modalities.
+    """
+    config = load_config(config_path)
+    init_logger(config.log_level)
+    config_freeze = config.model.freeze_backbone
+    config_label_type = config.dataset.label_type
+    config_backbones = config.model.backbones
+    config_modals = config.model.modals
+    config_fusion = config.model.fusion
+
+    model_label = (
+        f"distill--{generate_model_label(config_modals, config_freeze, config_label_type)}"
+    )
+    batch_size = 64 if config_freeze else 2
+
+    assert os.path.exists(f"./{teacher_checkpoint}/preprocessor")
+    assert config_fusion is not None
+
+    preprocessor = Preprocessor.from_pretrained(f"./{teacher_checkpoint}/preprocessor")
+    Path(f"./checkpoints/{model_label}").mkdir(exist_ok=True)
+    symlink(
+        f"./{teacher_checkpoint}/preprocessor",
+        f"./checkpoints/{model_label}/preprocessor",
+        target_is_directory=True,
+    )
+    symlink(
+        config_path,
+        f"./checkpoints/{model_label}/config.toml",
+    )
+    teacher_backbone = MultimodalBackbone.from_checkpoint(
+        f"{teacher_checkpoint}/backbones",
+    )
+    logger.info("load preprocessor and backbone from checkpoint")
+
+    train_dataset, dev_dataset, test_dataset = provide_meld_datasets(
+        config.dataset.path, preprocessor, label_type=config_label_type
+    )
+    train_data_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=LazyMultimodalInput.collate_fn,
+        pin_memory=True,
+    )
+    dev_data_loader = DataLoader(
+        dev_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=LazyMultimodalInput.collate_fn,
+        pin_memory=True,
+    )
+    test_data_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        collate_fn=LazyMultimodalInput.collate_fn,
+        pin_memory=True,
+    )
+    class_weights = torch.tensor(train_dataset.class_weights, dtype=torch.float32).cuda()
+
+    teacher_model = UnimodalModel(
+        teacher_backbone,
+        feature_size=config.model.feature_sizes[0],
+        num_classes=train_dataset.num_classes,
+        class_weights=class_weights,
+    ).cuda()
+
+    if os.path.exists(f"./{teacher_checkpoint}/stopper.json"):
+        load_best_model(teacher_checkpoint, teacher_model)
+        result = TrainingResult.auto_compute(teacher_model, test_data_loader)
+        logger.info("Test result(best model):")
+        result.print()
+
+    backbone = MultimodalBackbone(
+        *[AutoModel.from_pretrained(model_name) for model_name in config_backbones]
+    )
+    backbone.text_backbone = teacher_backbone.text_backbone
+    fusion_layer = gen_fusion_layer(config_fusion)
+    model = MultimodalModel(
+        backbone,
+        fusion_layer,
+        feature_sizes=config.model.feature_sizes,
+        num_classes=train_dataset.num_classes,
+        class_weights=class_weights,
+    ).cuda()
+
+    if not config_freeze:
+        model.unfreeze_backbone()
+
+    result: TrainingResult = train_and_eval_distill(
+        teacher_model,
+        model,
+        train_data_loader,
+        dev_data_loader,
+        test_data_loader,
+        num_epochs=200,
+        model_label=model_label,
+    )
+
+
+@app.command()
 def train(
     config_path: Path = Path("configs/default.toml"),
     checkpoint: Optional[Path] = None,
@@ -101,12 +226,10 @@ def train(
 
     model_label = generate_model_label(modals, freeze, label_type)
     batch_size = 64 if freeze else 2
-    if checkpoint is not None and os.path.exists(f"./{checkpoint}/preprocessor"):
+
+    if os.path.exists(f"./{checkpoint}/preprocessor"):
         preprocessor = Preprocessor.from_pretrained(f"./{checkpoint}/preprocessor")
-        backbone = MultimodalBackbone.from_checkpoint(
-            f"{checkpoint}/backbones",
-        )
-        logger.info("load preprocessor and backbone from checkpoint")
+        logger.info("load preprocessor from checkpoint")
     else:
         assert len(backbones) == len(
             modals
@@ -124,14 +247,21 @@ def train(
             ],
         )
         preprocessor.save_pretrained(f"./checkpoints/{model_label}/preprocessor")
-        config_link = Path(f"./checkpoints/{model_label}/config.toml")
-        if not config_link.exists():
-            logger.info(f"Create config link: {config_link}")
-            config_link.symlink_to(config_path.absolute())
 
+    if os.path.exists(f"./{checkpoint}/backbones"):
+        backbone = MultimodalBackbone.from_checkpoint(f"./{checkpoint}/backbones")
+        logger.info("load backbone from checkpoint")
+    else:
         backbone = MultimodalBackbone(
             *[AutoModel.from_pretrained(model_name) for model_name in backbones]
         )
+
+    checkpoint_dir = Path(f"./checkpoints/{model_label}")
+    checkpoint_dir.mkdir(parents=True, exist_ok=True)
+    symlink(
+        config_path,
+        f"./checkpoints/{model_label}/config.toml",
+    )
 
     train_dataset, dev_dataset, test_dataset = provide_meld_datasets(
         config.dataset.path, preprocessor, label_type=label_type
@@ -199,10 +329,10 @@ def train(
 
 @app.command()
 def inference(
+    checkpoint: Path,
     text: Optional[str] = None,
     audio_path: Optional[Path] = None,
     video_path: Optional[Path] = None,
-    checkpoint: Path = Path("."),
 ):
     config = load_config(f"{checkpoint}/config.toml")
     init_logger(config.log_level)
@@ -210,10 +340,7 @@ def inference(
     preprocessor = Preprocessor.from_pretrained(f"{checkpoint}/preprocessor")
     model_checkpoint = f"{checkpoint}/{find_best_model(checkpoint)}"
 
-    backbone = MultimodalBackbone.from_checkpoint(
-        f"{model_checkpoint}/backbones",
-        use_cache=False,
-    )
+    backbone = MultimodalBackbone.from_checkpoint(f"{model_checkpoint}/backbones")
     if config.model.fusion is None:
         model = UnimodalModel.from_checkpoint(model_checkpoint, backbone=backbone).cuda()
     else:
--- recognize/utils.py
+++ recognize/utils.py
@@ -15,9 +15,11 @@ from rich.progress import (
 )
 from safetensors.torch import load_file
 from torch.utils.data import DataLoader
+from transformers import get_linear_schedule_with_warmup
 
 from .evaluate import EarlyStopper, TrainingResult
 from .model.base import ClassifierModel
+from .module.loss import FeatureLoss, LogitLoss
 
 
 def save_checkpoint(
@@ -31,23 +33,29 @@ def save_checkpoint(
     epoch_checkpoint_dir.mkdir(parents=True, exist_ok=True)
     model.save_checkpoint(epoch_checkpoint_dir)
 
-    backbones_dir = epoch_checkpoint_dir / "backbones"
-    if not model.backbone.is_frozen:
-        # save and link backbone state_dict
-        backbones_dir.mkdir(parents=True, exist_ok=True)
-        for name, path in model.backbone.save().items():
-            (backbones_dir / f"{name}.safetensors").symlink_to(path)
-    else:
+    epoch_backbones_dir = epoch_checkpoint_dir / "backbones"
+    original_backbones_dir = checkpoint_dir / "backbones"
+    if model.backbone.is_frozen:
         # save and link original backbone state_dict
-        original_backbones_dir = checkpoint_dir / "backbones"
         if not original_backbones_dir.exists():
             original_backbones_dir.mkdir(parents=True)
             for name, path in model.backbone.save().items():
                 config_path = original_backbones_dir / name
                 model.backbone.get_submodule(name).config.save_pretrained(config_path)
                 (original_backbones_dir / f"{name}.safetensors").symlink_to(path)
+    else:
+        # save and link backbone state_dict
+        epoch_backbones_dir.mkdir(parents=True, exist_ok=True)
+        for name, path in model.backbone.save().items():
+            config_path = epoch_backbones_dir / name
+            model.backbone.backbones[name].config.save_pretrained(config_path)
+            (epoch_backbones_dir / f"{name}.safetensors").symlink_to(path)
 
-        backbones_dir.symlink_to("../backbones")
+        original_backbones_dir.unlink(missing_ok=True)
+        original_backbones_dir.symlink_to(
+            epoch_backbones_dir.relative_to(original_backbones_dir.parent),
+            target_is_directory=True,
+        )
 
     torch.save(optimizer.state_dict(), checkpoint_dir / "optimizer.pt")  # TODO: improve size
     stopper.save(checkpoint_dir / "stopper.json")
@@ -58,14 +66,7 @@ def load_model(checkpoint_dir: Path | str, model: ClassifierModel):
     model_state_dict = load_file(checkpoint_dir / "model.safetensors")
     model.load_state_dict(model_state_dict, strict=False)
     if (checkpoint_dir / "backbones").exists():
-        model.backbone.set_state_dicts(
-            {
-                name.split(".")[0]: load_file(checkpoint_dir / f"backbones/{name}")
-                for name in filter(
-                    lambda x: x.endswith(".safetensors"), os.listdir(checkpoint_dir / "backbones")
-                )
-            }
-        )
+        model.backbone.from_checkpoint(checkpoint_dir / "backbones")
 
 
 def find_best_model(checkpoint_dir: Path | str) -> int:
@@ -118,6 +119,7 @@ def init_torch():
 def train_epoch(
     model: ClassifierModel,
     optimizer: torch.optim.Optimizer,
+    scheduler: torch.optim.lr_scheduler.LambdaLR,
     train_data_loader: DataLoader,
     update_hook: Callable[[int, float], None] | None = None,
 ):
@@ -130,6 +132,7 @@ def train_epoch(
         assert loss is not None
         loss.backward()
         optimizer.step()
+        scheduler.step()
         loss_value_list.append(loss.item())
         loss_value = sum(loss_value_list) / len(loss_value_list)
         if update_hook is not None:
@@ -150,7 +153,13 @@ def train_and_eval(
 ):
     if optimizer is None:
         parameters = filter(lambda p: p.requires_grad, model.parameters())
-        optimizer = torch.optim.AdamW(parameters, lr=1e-5, weight_decay=0.1, amsgrad=True)
+        optimizer = torch.optim.AdamW(parameters, lr=1e-6, weight_decay=0.01)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=len(train_data_loader),
+        num_training_steps=len(train_data_loader) * num_epochs,
+    )
+
     if stopper is None:
         stopper = EarlyStopper()
     if test_data_loader is None:
@@ -158,6 +167,138 @@ def train_and_eval(
     if model_label is None:
         model_label = f"{model.__class__.__name__}-{id(model)}"
     # TODO: use checkpoint_dir
+    checkpoint_dir = Path(f"./checkpoints/{model_label}")
+    logger.add(f"./logs/{model_label}.txt")
+    logger.info(f"Train model [blue]{model_label}[/]. Save to [blue]{checkpoint_dir}[/]")
+
+    epoch_start = load_last_checkpoint(
+        checkpoint_dir,
+        model,
+        optimizer,
+        stopper,
+    )
+
+    best_epoch = stopper.best_epoch
+    batch_num = len(train_data_loader)
+    result: TrainingResult | None = None
+    with Progress(
+        "[red](Loss: {task.fields[loss]:.8f}, Accuracy: {task.fields[accuracy]:.2f}%, F1 Score: {task.fields[f1_score]:.2f}%)",
+        TextColumn("[progress.description]{task.description}"),
+        TaskProgressColumn("[progress.percentage](Epoch {task.completed}/{task.total})"),
+        BarColumn(bar_width=40),
+        TaskProgressColumn(f"[progress.percentage]({{task.fields[batch_index]}}/{batch_num})"),
+        TimeRemainingColumn(),
+    ) as progress:
+        loss_value = float("inf")
+        task = progress.add_task(
+            "[green]Training model",
+            total=num_epochs,
+            loss=loss_value,
+            accuracy=0,
+            f1_score=0,
+            completed=epoch_start + 1,
+            batch_index=0,
+        )
+        for epoch in range(epoch_start + 1, num_epochs):
+            train_epoch(
+                model,
+                optimizer,
+                scheduler,
+                train_data_loader,
+                lambda batch_index, loss_value: progress.update(
+                    task, loss=loss_value, batch_index=batch_index + 1
+                ),
+            )
+
+            if (epoch + 1) % eval_interval == 0 or epoch == num_epochs - 1:
+                result = TrainingResult.auto_compute(model, valid_data_loader)
+                test_f1_score = result.f1_score
+                test_accuracy = result.accuracy
+                if stopper.update(epoch=epoch, f1=test_f1_score):
+                    save_checkpoint(checkpoint_dir, epoch, model, optimizer, stopper)
+                    break
+
+                progress.update(task, f1_score=test_f1_score, accuracy=test_accuracy)
+                if stopper.best_epoch != best_epoch:
+                    best_epoch = stopper.best_epoch
+                    result.best_epoch = best_epoch
+                    save_checkpoint(checkpoint_dir, epoch, model, optimizer, stopper)
+                    logger.info(
+                        f"Epoch {best_epoch}: Better model found [red](Accuracy: {test_accuracy:.2f}%, F1 Score: {test_f1_score:.2f}%)",
+                    )
+                if epoch == num_epochs - 1:
+                    save_checkpoint(checkpoint_dir, epoch, model, optimizer, stopper)
+
+            progress.update(task, advance=1)
+
+    load_model(checkpoint_dir / str(best_epoch), model)
+    result = TrainingResult.auto_compute(model, test_data_loader)
+    result.best_epoch = best_epoch
+    result.save(f"{checkpoint_dir}/result.json")
+    return result
+
+
+def distill_epoch(
+    teacher_model: ClassifierModel,
+    student_model: ClassifierModel,
+    optimizer: torch.optim.Optimizer,
+    train_data_loader: DataLoader,
+    update_hook: Callable[[int, float], None] | None = None,
+):
+    teacher_model.eval()
+    student_model.train()
+    loss_value_list = []
+    for batch_index, batch in enumerate(train_data_loader):
+        optimizer.zero_grad()
+        with torch.no_grad():
+            teacher_embs = teacher_model.backbone(batch)[0]
+            teacher_pooled_embs = teacher_model.pool_embs(teacher_embs)
+            teacher_logits = teacher_model.classifier(teacher_pooled_embs)
+
+        student_embs_tuple = student_model.backbone(batch)
+        student_pooled_embs_tuple = student_model.pool_embs(*student_embs_tuple)
+        if student_pooled_embs_tuple[1] is None:
+            continue
+        student_output = student_model.classify(
+            student_model.fusion_layer(*student_pooled_embs_tuple), batch.labels
+        )
+
+        loss = (
+            LogitLoss()(student_output.logits, teacher_logits)
+            + FeatureLoss()(student_pooled_embs_tuple[1], teacher_pooled_embs)
+            # + FeatureLoss()(student_pooled_embs_tuple[2], teacher_pooled_embs)
+            + student_output.loss
+        )
+        loss.backward()
+        optimizer.step()
+        loss_value_list.append(loss.item())
+        loss_value = sum(loss_value_list) / len(loss_value_list)
+        if update_hook is not None:
+            update_hook(batch_index, loss_value)
+
+
+def train_and_eval_distill(
+    teacher_model: ClassifierModel,
+    model: ClassifierModel,
+    train_data_loader: DataLoader,
+    valid_data_loader: DataLoader,
+    test_data_loader: DataLoader | None = None,
+    *,
+    optimizer: torch.optim.Optimizer | None = None,
+    stopper: EarlyStopper | None = None,
+    num_epochs: int = 100,
+    model_label: str | None = None,
+    eval_interval: int = 1,
+):
+    if optimizer is None:
+        parameters = filter(lambda p: p.requires_grad, model.parameters())
+        optimizer = torch.optim.AdamW(parameters, lr=1e-5, weight_decay=0.1)
+    if stopper is None:
+        stopper = EarlyStopper()
+    if test_data_loader is None:
+        test_data_loader = valid_data_loader
+    if model_label is None:
+        model_label = f"{model.__class__.__name__}-{id(model)}"
     checkpoint_dir = Path(f"./checkpoints/{model_label}")
     checkpoint_dir.mkdir(parents=True, exist_ok=True)
     logger.add(f"./logs/{model_label}.txt")
@@ -192,7 +333,8 @@ def train_and_eval(
             batch_index=0,
         )
         for epoch in range(epoch_start + 1, num_epochs):
-            train_epoch(
+            distill_epoch(
+                teacher_model,
                 model,
                 optimizer,
                 train_data_loader,
