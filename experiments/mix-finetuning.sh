0984ab42bb82ee63bdf567112bc844043edfcb4f
zrr1999 <2742392377@qq.com>
2024-09-14 23:10:11 +0800

    👷 ci: add simple train workflow

---
+++ experiments/mix-finetuning.sh
@@ -0,0 +1,5 @@
+uv run emotion-recognize train configs/T--T.toml configs/dataset/MELD--E.toml --seed 114
+uv run emotion-recognize train configs/A--T.toml configs/dataset/MELD--E.toml --seed 114
+uv run emotion-recognize train configs/V--T.toml configs/dataset/MELD--E.toml --seed 114
+uv run emotion-recognize train configs/T+A--T.toml configs/dataset/MELD--E.toml --seed 114
+uv run emotion-recognize train configs/T+A+V--T.toml configs/dataset/MELD--E.toml --seed 114
