766cfec72dd192d3dc534ac07ba9dc280b408458
zrr1999 <2742392377@qq.com>
2024-12-01 19:05:05 +0800

    🐛 fix: fix deterministic

--- src/recognize_cli/cli_recognize.py
+++ src/recognize_cli/cli_recognize.py
@@ -49,10 +49,11 @@ def seed_everything(seed: int | None = None):
     logger.info(f"Set seed to {seed}")
     random.seed(seed)
     os.environ["PYTHONHASHSEED"] = str(seed)
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
+    torch.use_deterministic_algorithms(True)
     torch.backends.cudnn.benchmark = False
     return seed
 
